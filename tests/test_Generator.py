9603743b1054ffdd7d573c9ed458109e5e0c3670
Adrien Felipe <adrien@macnificos.com>
2020-04-11 11:38:59 +0200

    Linear event

--- tests/test_Generator.py
+++ tests/test_Generator.py
@@ -2,8 +2,8 @@ import random
 import unittest
 from typing import Callable
 
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 from Generator import Generator
 from events.History import History
@@ -71,15 +71,15 @@ class MyTestCase(unittest.TestCase):
     @staticmethod
     def test_pattern_search():
         cause_function: Callable[[History], float] = lambda history: \
-            np.sin(history.get_event(0) * np.pi / 4)
+            np.sin(history.get_event(0) * np.pi / 2)
 
         dataset = Generator(cause_function) \
             .add_noise_linear(start=-2, step=0.5) \
             .add_cause_linear() \
-            .generate(4) \
-            .astype(int)
+            .generate() \
+            .round(1)
 
-        expected = pd.DataFrame({'E': [0, 9, 0, 0], 'N': [7, 0, 0, 2], 'X': [0, 0, 21, 0]})
+        expected = pd.DataFrame({'E': [1, 2, 3], 'N': [-1.5, -1, -0.5], 'X': [1, 0, -1]})
         pd.testing.assert_frame_equal(expected, dataset, check_dtype=False)
 
     def test_search(self):
